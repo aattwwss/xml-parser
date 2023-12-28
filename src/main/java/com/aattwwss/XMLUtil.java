ecba4163e856277e3a29fdaad7ec0c0a9c4491f5
alvin tan <alvintan95@gmail.com>
2023-12-28 15:27:05 +0800

    add javadoc

--- src/main/java/com/aattwwss/XMLUtil.java
+++ src/main/java/com/aattwwss/XMLUtil.java
@@ -1,6 +1,16 @@
 package com.aattwwss;
 
+/**
+ * Utility class for handling XML.
+ */
 public class XMLUtil {
+
+    /**
+     * Checks if the given string is valid XML.
+     *
+     * @param input The string to check.
+     * @return True if the string is valid XML, false otherwise.
+     */
     public static boolean isValidXML(String input) {
         try {
             Node root = Parser.parse(input);
